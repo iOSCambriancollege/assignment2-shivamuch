d52fe59c1e324e514d1bb77ba155e9609318b632
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-02-01 22:56:53 -0500

    Class for detail controller to manage the working

--- A2_WebServices/DetailsController.swift
+++ A2_WebServices/DetailsController.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class DetailsController: UIViewController {
+    var dogName: String!
+    let URL: String = "https://dog.ceo/api/breed/"
+    @IBOutlet weak var dogMainImage: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url: String = URL + dogName + "/images"
+            DogAPIHelper.fetchImage(url: url ) {dogImage in
+
+                for _ in 0...1000000{
+                    continue
+                }
+                do{
+                    try
+                    self.dogMainImage.image = UIImage(data: NSData(contentsOf: NSURL(string: dogImage[0])! as URL) as Data)
+                }catch let error{
+                    print(error)
+                }
+          }
+    }
