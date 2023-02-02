e1d2083f9881e76560b708734d3e379e836b6e10
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-02-01 22:58:28 -0500

    Made changes in the dog cell to view the image outlets

--- A2_WebServices/DogCell.swift
+++ A2_WebServices/DogCell.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+import UIKit
+
+
+class DogTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var dogName: UILabel!
+    @IBOutlet weak var dogImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
